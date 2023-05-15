9a0ff8d8ec01a5af7b3cf42ec566f3926d3afc28
Nicolas Vieira <macneckezs@gmail.com>
2023-05-15 17:37:41 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocaTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+    body.classList.toggle("modo-escuro");
+    if (modoEscuroEstaAtivo){
+        body.classList.remove("modo-escuro");
+        imagemBotaoTrocaTema.setAttribute("src", "./src/imagens/sun.png");
+    } else {
+        body.classList.add("modo-escuro");
+        imagemBotaoTrocaTema.setAttribute("src", './src/imagens/moon.png');
+    };
+});
